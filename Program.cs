ab94c97d5e4379ba2991b4c0cc8072ba987ab221
Subtype3860 <booravoi@gmail.com>
2022-11-29 16:40:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace MyTexterBot
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+ 
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+ 
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("5681894086:AAGto1JxL-s7KNqqbJOMJnqoFUVHSpyPGM4"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
